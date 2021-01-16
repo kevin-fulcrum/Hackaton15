80dbd12858be7f4abef76df0f539f6d5096ca044
kevin-fulcrum <kevinariassistemas@gmail.com>
2021-01-16 09:37:56 -0500

    [feacture]-carga

---
+++ src/component/button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text} from 'react-native'
+
+const Button=()=>{
+  return(
+    <Text>buenos dias</Text>
+  )
+};
+
+export default Button;
---
+++ src/component/screens/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {} from 'react-native'
+import Button from '../button/Button'
+
+const Welcome = ()=>{
+    return(
+        <Button></Button>
+    )
+};
+
+export default Welcome;
